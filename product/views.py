4b673fd59779616d586f4bf7d2c853b1352af285
francobenitez <franco.benitez@hogarthww.com>
2022-01-15 16:11:53 -0300

    feat: develop product view

--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Product
@@ -15,4 +16,18 @@ class LatestProductsList(APIView):
     ## Many == True because we have more than one object
     serializer = ProductSerializer(products, many=True)
 
+    return Response(serializer.data)
+
+class ProductDetail(APIView):
+  def get_object(self, category_slug, product_slug):
+    try:
+      return Product.objects.filter(
+        category__slug=category_slug
+        ).get(slug=product_slug)
+    except Product.DoesNotExist:
+      raise Http404
+
+  def get(self, request, category_slug, product_slug, format=None):
+    product = self.get_object(category_slug, product_slug)
+    serializer = ProductSerializer(product)
     return Response(serializer.data)
